9fb38f14948f813a29daaba911848bd69dde3068
DAYLY CRYSTEL PUC POOT <250300522@ucaribe.edu.mx>
2025-09-18 07:18:18 -0500

    Update about.js

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,8 +1,13 @@
 $(document).is(function(){
  if $('.user-intro h1').Length({
-  anime.timeline(loop:false}).Morphext({
-    animation: "flip",
-    separator: ",",
-    speed: 2000
-  });
+  anime.timeline(loop:false})
+  .add({
+    targets:´.user-intro h1´,
+    opacity:[0,1],
+    translateY: [2-20px", "0px2],
+    easing: "easeOutExpo2,
+    duration. 2000
+
+      });
+   }
 });
